d7c0b70b52d33e29fbd5119c5d0d1035686c27e8
veresmar <veres.maria.dev@gmail.com>
2023-04-17 11:13:08 +0400

    upd

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
+const addTaskString = document.getElementById('add-task');
+const addTaskBtn = document.getElementById('add-btn');
+const doneCheck = document.getElementById('done');
+const deleteX = document.getElementById('del-btn');
+const deleteDone = document.getElementById('del-done-btn');
+const deleteAll = document.getElementById('del-all-btn');
 
 
-fetch("http://24api.ru/rest-user", {
-    "method": "GET"
-})
-    .then(data => data.json())
-    .then(data => console.log(data))
+// получение инфо о пользователе (?)
+// fetch("http://24api.ru/rest-user", {
+//     "method": "GET"
+// })
+//     .then(data => data.json())
+//     .then(data => console.log(data))
 
-
+// Создание пользователя
 const user = {
     "id": null,
     "username": "Maria",
@@ -17,8 +24,6 @@ const user = {
     "password_hash": "12345678"
 }
 
-// POST
-
 fetch("http://24api.ru/rest-user", {
     method: "POST",
     headers: {
@@ -28,14 +33,15 @@ fetch("http://24api.ru/rest-user", {
 })
     .then(data => console.log(data))
 
-// создаем задание (const идет перед fetch)
+
+// Создание задачи
+
 const user1 =  {
   "id": null,
-  "name": "купить кофе",
+  "name": `${addTaskString.value}`,
   "isDone": 1,
-  "user_id": 5
+  "user_id": 26
 }
-
 
 fetch("http://24api.ru/rest-todo", {
     method: "POST",
@@ -45,11 +51,79 @@ fetch("http://24api.ru/rest-todo", {
     body: JSON.stringify(user1)
 })
     .then(data => console.log(data))
+    // .then(data => createNewTask(data))
 
-    
-const addCaseBtn = document.getElementById('add-case');
-const doneCheck = document.getElementById('done');
-const deleteX = document.getElementById('del-btn');
-const deleteDone = document.getElementById('del-done-btn');
-const deleteAll = document.getElementById('del-all-btn');
 
+// Удаление одной задачи
+
+// fetch("http://24api.ru/rest-todo/26", {
+//     method: "DELETE",
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(user1)
+// })
+//     .then(data => console.log(data))
+
+
+
+
+// Создание задачи
+
+addTaskBtn.addEventListener('click', function f1() {
+  if (addTaskString.value.length == 0){
+    addTaskString.value = 'please add some task';
+  }
+  else {
+    fetch("http://24api.ru/rest-todo", {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user1)
+})
+    .then(data => console.log(data))
+  }
+})
+
+//получение инфо о задаче
+const task =  {
+  "id": null,
+  "name": `${addTaskString.value}`,
+  "isDone": 1,
+  "user_id": 26
+}
+
+fetch("http://24api.ru/rest-todo/26", {
+    method: "GET",
+    headers: {
+        'Content-Type': 'application/json'
+    }
+    // body: JSON.stringify(user1)
+})
+    .then(data => console.log(data))
+
+
+// Удаление задачи
+
+// deleteX.addEventListener('click'()=> {
+//   fetch("http://24api.ru/rest-todo/", {
+//     method: "DELETE",
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(user1)
+// })
+//     .then(data => console.log(data))
+
+// })
+
+
+// function createNewTask(){
+//   const newTask = document.createElement('input');
+//   newTask.type = 'checkbox';
+//   const newTaskText = document.createElement('label');
+//   newTaskText.value = addTaskString.value;
+//   const deleteX = document.createElement('button')
+//   deleteX.innerHTML = '❌';
+// }
